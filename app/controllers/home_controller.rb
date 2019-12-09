3b7700559df59369017abddbb313eeed51222a22
Scott Fleming <sdflem@gmail.com>
2019-12-09 16:31:04 -0600

    WIP s10bonus_solution

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    poems = current_user.poems
+    respond_to do |format|
+      format.html { render :index, locals: { poems: poems } }
+    end
   end
 
 end
