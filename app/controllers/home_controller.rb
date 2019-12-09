d6e48f08c699760e456373a859fb250ada1cb8b0
Scott Fleming <sdflem@gmail.com>
2019-12-09 16:32:27 -0600

    HOTFIX s10bonus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    respond_to do |format|
+      format.html { render :index }
+    end
   end
 
 end
